519ee035355a30c70d8a4d6366995b9f76ebfbc9
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-30 11:55:03 +0200

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./View/Pages/HomePage";
+import CityPage from "./View/Pages/CityPage";
+import AboutPage from "./View/Pages/AboutPage";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/citypage" element={<CityPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
