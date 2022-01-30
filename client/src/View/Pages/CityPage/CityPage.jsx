ac20b418934871f78dd53a7ef8ba5fdb362176ca
svetlanagaydey <lana84@ukr.net>
2022-01-30 18:49:49 +0200

    request to databace added

--- client/src/View/Pages/CityPage/CityPage.jsx
+++ client/src/View/Pages/CityPage/CityPage.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 import CitiesList from './CitiesList';
-import './CityPage.css'
+import './CityPage.css';
+import cultourApi from "../../../Api/cultourApi";
 
 const CityPage = () => {
-	//const currentCity = props
+
+	const [places, setPlaces] = useState([]);
+	//const [curCity, setCurCity] = useState("");
+
 	const currentCity = 'Haifa';
-	const currentList = CitiesList.filter((places) => places.location==="Haifa")
+	const currentList = CitiesList.filter((places) => places.location==="Haifa");
+
+	useEffect(() => {
+		getReq();
+	}, [places]);
+
+	const getReq = async () => {
+		try {
+			const { requestData } = await cultourApi.get("sites");
+			setPlaces(requestData);
+			console.log(places)
+		} catch (e) {
+			console.error(e.message)
+		}
+	};
+	console.log(places)
+	
 	return (
 		<div className="container">
-			<h2>{currentCity}</h2>
-			{currentList.map((place) => {
+			<h2 className="city">{currentCity}</h2>
+			{currentList.map((place, ind) => {
 				return (
-					<div className="place">
+					<div className="place" key={`place${ind}`}>
 						<h3 className="place-name">{place.name}</h3>
-						{place.description.map((par) => {
-								return <p>{par}</p>
-						})}
+						<div className="place-description">{place.description}</div>
+						{/* {place.description.map((par, index) => {
+								return <p key={index}>{par}</p>
+						})} */}
 							<img className="place-image" src={place.image} alt=""/>
 					</div>
 				)
