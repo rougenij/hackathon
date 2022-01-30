a7ef63a2ad704ecf987eeebf83b875b8a0567066
svetlanagaydey <lana84@ukr.net>
2022-01-30 12:00:56 +0200

    css file added

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,8 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Cultour</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
   return <div className="App"></div>;
->>>>>>> 845fe121674b26ed08d5111344ca5bca9fb259e8
 }
 
 export default App;
